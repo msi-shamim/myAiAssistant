646950875b54a6e84032850d757e0d79ba1f3fc8
faria-incrementsinc <saima.faria@incrementsinc.com>
2024-05-07 18:42:35 +0600

    fix: terminal close upon execution fix, whisper custom files added

--- llm_integration.py
+++ llm_integration.py
@@ -22,3 +22,19 @@ def query_gpt(text):
     return completion.choices[0].message.content
 
 # query_gpt("open a file notes and close it")
+
+
+def query_gpt_do_task(text):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        temperature=1,
+        messages=[
+            {
+                "role": "user",
+                "content": f"separate the commands for the following sentence, {text}"
+                # "content": f"{text}"
+            }
+        ]
+    )
+    print(completion.choices[0].message.content)
+    return completion.choices[0].message.content
